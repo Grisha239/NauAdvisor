f0b984962b2858739726d6c9f25f5525ee0a9606
GRMSI <grisha.239@gmail.com>
2024-05-28 12:30:28 +0300

    Docker

--- main.py
+++ main.py
@@ -45,14 +45,16 @@ def answer_chat(message):
     response_json = json.loads(response.text.replace("\'", '\"'))
     bot.send_message(chat_id=message.chat.id, text=f"Yandex answer:\n"
                                                    f"{response_json['result']['alternatives'][0]['message']['text']}")
-
-    answer_openai = client.chat.completions.create(
-        model=openaiModel,
-        messages=[
-            {'role': 'user', 'content': text}
-        ]
-    )
-    bot.send_message(chat_id=message.chat.id, text=f"OpenAI answer:\n{answer_openai.choices[0].message}")
+    try:
+        answer_openai = client.chat.completions.create(
+            model=openaiModel,
+            messages=[
+                {'role': 'user', 'content': text}
+            ]
+        )
+        bot.send_message(chat_id=message.chat.id, text=f"OpenAI answer:\n{answer_openai.choices[0].message}")
+    except Exception as exception:
+        bot.send_message(chat_id=message.chat.id, text=f"OpenAI answer:\n{exception}")
 
 
 if __name__ == "__main__":
