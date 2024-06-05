d264cb8161c2f92265af4b09612dae1a32f04262
GRMSI <grisha.239@gmail.com>
2024-06-05 11:35:31 +0300

    import redis and test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import telebot
 from openai import OpenAI
 import requests
+import redis
 
 load_dotenv()
 
@@ -19,6 +20,11 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 # Telegram bot
 BotToken = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BotToken)
+
+# Redis database
+redis_host = os.getenv("REDIS_HOST")
+redis_port = os.getenv("REDIS_PORT")
+database = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 
 @bot.message_handler(commands=['gpt'])
@@ -52,7 +58,10 @@ def answer_chat(message):
                 {'role': 'user', 'content': text}
             ]
         )
+        conversation = database.get(message.from_id)
+        database.set(message.from_id, conversation + message.from_id + answer_openai.choices[0].message.content)
         bot.send_message(chat_id=message.chat.id, text=f"OpenAI answer:\n{answer_openai.choices[0].message.content}")
+        bot.send_message(chat_id=message.chat.id, text=f"Conversation:\n{conversation}")
     except Exception as exception:
         bot.send_message(chat_id=message.chat.id, text=f"OpenAI exception:\n{exception}")
 
