32d78e527338d5886b33bf6ad2a17dcc1a194e00
GRMSI <grisha.239@gmail.com>
2024-06-17 12:24:49 +0300

    +

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ redis_port = os.getenv("REDIS_PORT")
 database = redis.Redis(host=redis_host, port=redis_port, db=0)
 
 
-@bot.message_handler(func=lambda message: message.text & message.text[0] != "/")
+@bot.message_handler(func=lambda message: message.text[0] != "/")
 def answer_chat(message):
     text = message.text
 
